ab157c6fbe62480a7110da199c3ac671523df371
Justin Yao Du <justinyaodu@gmail.com>
2020-09-08 13:54:54 -0700

    Refactor and reorder classes

--- latexclippings.py
+++ latexclippings.py
@@ -7,76 +7,15 @@ import tempfile
 import textwrap
 
 
-class _LatexChunk:
-    """Represent a section of logically related LaTeX source code."""
-    
-    # Precedes the log output for each chunk.
-    CHUNK_HEADER = "SVGFROMLATEX CHUNK HEADER"
-
-    def __init__(self, name, lines, new_page=False, clipping_index=None):
-        self.name = name
-        self.clipping_index = clipping_index
-
-        self.lines = [r"\typeout{" + __class__.CHUNK_HEADER + "}"]
-        self.source_start = 1
-        if new_page:
-            self.lines.append(r"\newpage")
-            self.source_start += 1
-        self.lines.extend(lines)
-
-    def __len__(self):
-        return len(self.lines)
-
-    def __str__(self):
-        return "\n".join(self.lines)
-
-
-class LatexError(Exception):
-    """Raised when an error occurs while rendering LaTeX."""
-
-    def __init__(self, clipping_index, location, line_num, error_msg, context):
-        # Index of the clipping this error occurred in.
-        self.clipping_index = clipping_index
-
-        # Human-readable chunk name.
-        self.location = location
-
-        # One-indexed line number of the error.
-        self.line_num = line_num
-
-        self.error_msg = error_msg
-
-        # Point out the error line and show a few adjacent lines.
-        self.context = context
-
-        super().__init__(str(self))
-
-    def __str__(self):
-        return "".join([
-            f"{self.location}, line {self.line_num}: {self.error_msg}\n",
-            self.context,
-        ])
-
-
-class LatexClipping:
-    """Represent a rendered LaTeX clipping."""
-
-    def __init__(self, latex):
-        self.latex = latex
-        self.log = None
-
-
 class LatexFile:
-    """Represent the source of a LaTeX document."""
-
-    _error_regex = "".join([
-        r"^! (?P<error_msg>.*)[\n]",
-        r"l\.(?P<line_num>[0-9]+) (?P<line_contents>.*)$",
-    ])
+    """Represent a LaTeX document, composed of a preamble, clippings,
+    and additional pages used for internal measurements.
+    """
 
     def __init__(self, clippings, preamble=r"\documentclass{minimal}"):
         self.clippings = [LatexClipping(c) for c in clippings]
         self._init_chunks(preamble, clippings)
+        self._render()
 
     def _init_chunks(self, preamble, clippings):
         self.chunks = []
@@ -121,14 +60,64 @@ class LatexFile:
     def __str__(self):
         return "\n".join(str(chunk) for chunk in self.chunks)
 
-    def _parse_log(self, log):
+    def _render(self):
+        """Render each clipping as a SVG."""
+
+        with tempfile.TemporaryDirectory() as temp_dir:
+            temp_dir = Path(temp_dir)
+            pdf_path = self._pdflatex(temp_dir)
+
+            # TODO
+            subprocess.run(["firefox", shlex.quote(str(pdf_path))])
+            input()
+
+    def _pdflatex(self, working_dir):
+        """Convert this LaTeX document to a PDF. Raise LatexError if
+        pdflatex is unsuccessful.
+        """
+
+        tex_file = working_dir / "clippings.tex"
+        with open(tex_file, "w") as f:
+            f.write(str(self))
+
+        pdflatex_process = subprocess.run(
+            [
+                "pdflatex",
+                "-halt-on-error",
+                shlex.quote(str(tex_file))
+            ],
+            cwd=working_dir,
+            text=True,
+            capture_output=True,
+        )
+
+        self._parse_pdflatex_log(pdflatex_process.stdout)
+
+        try:
+            pdflatex_process.check_returncode()
+        except subprocess.CalledProcessError as e:
+            raise ValueError(pdflatex_process.stdout) from e
+
+        return working_dir / "clippings.pdf"
+
+    _pdflatex_error_regex = "".join([
+        r"^! (?P<error_msg>.*)[\n]",
+        r"l\.(?P<line_num>[0-9]+) (?P<line_contents>.*)$",
+    ])
+
+    def _parse_pdflatex_log(self, log):
+        """Parse the pdflatex log, assigning log sections to the
+        corresponding LatexClippings. Raise LatexError for any error
+        messages encountered.
+        """
+
         log_sections = log.split(_LatexChunk.CHUNK_HEADER + "\n")
 
         # Skip the initialization output (before start of preamble).
         log_sections = log_sections[1:]
 
         for _ in zip(self.chunks, log_sections, itertools.count()):
-            chunk, log_section, index = _
+            chunk, log_section, chunk_index = _
             clipping = None
 
             if chunk.clipping_index is not None:
@@ -139,14 +128,15 @@ class LatexFile:
                 if clipping.log is None:
                     clipping.log = log_section
 
-            match = re.search(__class__._error_regex, log_section, re.MULTILINE)
+            match = re.search(__class__._pdflatex_error_regex, log_section,
+                    re.MULTILINE)
             if match:
                 groupdict = match.groupdict()
 
                 # Get zero-indexed line numbers.
                 file_line_num = int(groupdict['line_num']) - 1
                 chunk_line_num = (file_line_num
-                        - sum(len(c) for c in self.chunks[:index]))
+                        - sum(len(c) for c in self.chunks[:chunk_index]))
 
                 context_lines = []
                 context_dist = 2
@@ -162,36 +152,65 @@ class LatexFile:
 
                 display_line_num = chunk_line_num - chunk.source_start + 1
 
-                raise LatexError(index, chunk.name, display_line_num,
-                        groupdict['error_msg'], context)
+                raise LatexError(chunk.clipping_index, chunk.name,
+                        display_line_num, groupdict['error_msg'], context)
 
-    def _render(self):
-        with tempfile.TemporaryDirectory() as temp_dir:
-            temp_dir = Path(temp_dir)
 
-            tex_file = temp_dir / "clippings.tex"
+class LatexClipping:
+    """Represent a rendered LaTeX clipping."""
 
-            with open(tex_file, "w") as f:
-                f.write(str(self))
+    def __init__(self, latex):
+        self.latex = latex
+        self.log = None
 
-            pdflatex_process = subprocess.run(
-                [
-                    "pdflatex",
-                    "-halt-on-error",
-                    shlex.quote(str(tex_file))
-                ],
-                cwd=temp_dir,
-                text=True,
-                capture_output=True,
-            )
 
-            self._parse_log(pdflatex_process.stdout)
+class LatexError(Exception):
+    """Raised when an error occurs while rendering LaTeX."""
 
-            try:
-                pdflatex_process.check_returncode()
-            except subprocess.CalledProcessError as e:
-                raise ValueError(pdflatex_process.stdout) from e
+    def __init__(self, clipping_index, location, line_num, error_msg, context):
+        # Index of the clipping this error occurred in, or None if the
+        # error occurred elsewhere (e.g. in the preamble).
+        self.clipping_index = clipping_index
 
-            # TODO
-            subprocess.run(["firefox", shlex.quote(str(temp_dir / "clippings.pdf"))])
-            input()
+        # Human-readable chunk name.
+        self.location = location
+
+        # One-indexed line number of the error.
+        self.line_num = line_num
+
+        self.error_msg = error_msg
+
+        # Point out the error line and show a few adjacent lines.
+        self.context = context
+
+        super().__init__(str(self))
+
+    def __str__(self):
+        return "".join([
+            f"{self.location}, line {self.line_num}: {self.error_msg}\n",
+            self.context,
+        ])
+
+
+class _LatexChunk:
+    """Represent a section of logically related LaTeX source code."""
+
+    # Precedes the log output for each chunk.
+    CHUNK_HEADER = "SVGFROMLATEX CHUNK HEADER"
+
+    def __init__(self, name, lines, new_page=False, clipping_index=None):
+        self.name = name
+        self.clipping_index = clipping_index
+
+        self.lines = [r"\typeout{" + __class__.CHUNK_HEADER + "}"]
+        self.source_start = 1
+        if new_page:
+            self.lines.append(r"\newpage")
+            self.source_start += 1
+        self.lines.extend(lines)
+
+    def __len__(self):
+        return len(self.lines)
+
+    def __str__(self):
+        return "\n".join(self.lines)
